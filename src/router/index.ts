c2b930b320e034615475cc44400f0ca78cc74c1f
Rain Yohe <zty8212000@gmail.com>
2024-12-04 12:05:17 -0500

    Add redirection to home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,10 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: "/",
+            redirect: "/aoc2024",
+        },
+        {
             path: "/aoc2024",
             name: "About",
             component: AboutView,

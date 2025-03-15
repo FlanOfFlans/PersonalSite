b580b94ce460fbf394ddae710a3c5872552eacf2
Rain Yohe <rain.yohe@roushtech.net>
2025-03-15 19:49:54 -0400

    Basic scaffolding for pendulum demo

---
+++ src/components/diffeq/PendulumSim.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+<script setup lang="ts">
+    import { Application } from 'pixi.js';
+    import { useTheme } from 'vuetify';
+    import { computed } from 'vue';
+
+    const theme = useTheme();
+    const backgroundColor = computed(() => theme.current.value.colors.background);
+
+    const app = new Application();
+    await app.init({ background: backgroundColor.value });
+
+    
+</script>

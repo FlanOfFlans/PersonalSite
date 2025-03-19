5d1a19b7dcbcd0f2e936a15e1aaab318b504e7ce
Rain Yohe <rain.yohe@roushtech.net>
2025-03-19 18:03:19 -0400

    Try adding commonJS plugin?

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import commonjs from 'vite-plugin-commonjs'
 import fs from "fs";
 import path from "node:path";
 
@@ -10,6 +11,7 @@ export default defineConfig(({ command, mode }) => { return {
     plugins: [
         vue(), 
         vueDevTools(),
+        commonjs(),
         {
             name: 'build-page-manifest',
             buildStart(options) {
